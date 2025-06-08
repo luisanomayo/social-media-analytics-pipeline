c17794691dcd85aba0a95e19f20fb0a32107871f
Ame Avan-Nomayo <36841345+luisanomayo@users.noreply.github.com>
2025-06-08 20:19:42 +0100

    inclusion of local postgres serve connection details

--- src/config.py
+++ src/config.py
@@ -9,3 +9,13 @@ TWITTER_BEARER_TOKEN = os.getenv("TWITTER_BEARER_TOKEN")
 
 #service account credentials
 SERVICE_ACCOUNT_JSON = os.getenv("SERVICE_ACCOUNT_CREDENTIALS")
+
+#spreadsheet ID
+SPREADSHEET_KEY = os.getenv("SPREADSHEET_KEY")
+
+#PGSQL database connection
+PG_HOST = os.getenv("PG_HOST")
+PG_PORT = os.getenv("PG_PORT")
+PG_USER = os.getenv("PG_USER")
+PG_PASS = os.getenv("PG_PASS")
+PG_DB = os.getenv("PG_DB")
